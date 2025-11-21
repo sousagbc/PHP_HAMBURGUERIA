c26cb1d912ed7415c7071f9cc370a00bff03c5b6
Lielson Marques <lielsonml@gmail.com>
2025-11-21 16:19:55 -0300

    Add files via upload

---
+++ indexadm.php
@@ -0,0 +1,22 @@
+<?php
+// admin/index.php
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin - Painel</title>
+    <style>
+        body { font-family: Arial; background:#f2f2f2; padding:40px; }
+        a { display:block; padding:12px; background:#333; color:white; width:200px; text-align:center; text-decoration:none; margin-bottom:10px; }
+        a:hover { background:#555; }
+    </style>
+</head>
+<body>
+
+<h1>Painel Administrativo</h1>
+
+<a href="pedidos.php">Gerenciar Pedidos</a>
+
+</body>
+</html>
