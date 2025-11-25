196f6e8f48c794e6c908ec3e1b2174c4a91b2243
lielsonml <lielsonml@gmail.com>
2025-11-24 22:57:55 -0300

    .

--- admin/indexadm.php
+++ admin/indexadm.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Admin - Painel</title>
+    <link rel="stylesheet" href="../style.css">
     <style>
         body { font-family: Arial; background:#f2f2f2; padding:40px; }
         a { display:block; padding:12px; background:#333; color:white; width:200px; text-align:center; text-decoration:none; margin-bottom:10px; }
