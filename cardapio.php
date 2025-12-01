3e3bdcda6a0068b63596a5b7256b9fad27646016
gabriel sousa <sousagbc@hotmail.com>
2025-12-01 13:54:28 -0300

    Ajustes Gerais

--- cardapio.php
+++ cardapio.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>O Burgues - Nosso Cardápio</title>
-    <link rel="icon" href="imagens/logo_burgues_laranja_2 (1).png" type="image/png">
-    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/logo.png (1).png" type="image/png">
+    <link rel="stylesheet" href="cardapio.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
@@ -20,28 +20,28 @@
     <section id="cardapio">
     <div id="corpo">
         <div id="hamburguer">
-            <img id="foto" src="imagens/456881261_1724569375019748_4628731805975508229_n.jpg"  alt="American Chicken">
+            <img id="foto" src="images/imagens/456881261_1724569375019748_4628731805975508229_n.jpg"  alt="American Chicken">
             <h3>American Chicken</h3>
             <p>Frango Empanado, Queijo Mussarela, Alface Americana, Picles de Pepino, Molho a sua escolha e Tudo isso no pão brioche</p>
             <strong>R$ 29,90</strong>
         </div>
 
         <div id="hamburguer">
-            <img id="foto" src="imagens/457018579_519335344006953_2579318147507496788_n.jpg" alt="Burger Clássico">
+            <img id="foto" src="images/imagens/457018579_519335344006953_2579318147507496788_n.jpg" alt="Burger Clássico">
             <h3>Clássico</h3>
             <p>Carne 180g, cheddar duplo, bacon crocante, cebola caramelizada.</p>
             <strong>R$ 34,90</strong>
         </div>
 
         <div id="hamburguer">
-            <img id="foto" src="imagens/457257238_865974362133289_2550781275651349189_n.jpg" alt="Costela Creamy">
+            <img id="foto" src="images/imagens/457257238_865974362133289_2550781275651349189_n.jpg" alt="Costela Creamy">
             <h3>Costela Creamy</h3>
             <p>Blend de 180g de Costela, Cream cheese, Alho Crocante, Cebola no shoyu, Mollho a sua escolha,Tudo montado no pão brioche</p>
             <strong>R$ 31,90</strong>
         </div>
 
         <div id="hamburguer">
-            <img id="foto" src="imagens/457022564_485837667570122_1376005797323809782_n.jpg" alt="Costela Angus">
+            <img id="foto" src="images/imagens/457022564_485837667570122_1376005797323809782_n.jpg" alt="Costela Angus">
             <h3>Costela Angus</h3>
             <p>180g De Costela Bovina Angus, Queijo
              Cheddar, Alface Americana, Bacon Em Cubos, Pico De Gallo E Molho À Sua Escolha No Pão Brioche.</p>
@@ -49,14 +49,14 @@
         </div>
 
         <div id="hamburguer">
-            <img id="foto" src="imagens/457281214_1243798510113364_8484507634713217653_n.jpg" alt="Cheese crispy">
+            <img id="foto" src="images/imagens/457281214_1243798510113364_8484507634713217653_n.jpg" alt="Cheese crispy">
             <h3>Cheese crispy</h3>
             <p>110g do blend burguês, queijo empanado crocante da seara, farofa de bacon, molho a sua escolha no pão de brioche.</p>
             <strong>R$ 41,90</strong>
         </div>
 
         <div id="hamburguer">
-            <img id="foto" src="imagens/457132184_8125801310792444_2026918450704921741_n.jpg" alt="Frango Cream Cheese">
+            <img id="foto" src="images/imagens/457132184_8125801310792444_2026918450704921741_n.jpg" alt="Frango Cream Cheese">
             <h3>Frango Cream Cheese</h3>
             <p>Frango Empanado, Queijo Muçarela, Cream Cheese, Alho
             Crocante, Cebola Roxa, Tomate E Alface Americana No Pão brioche</p>
@@ -64,28 +64,28 @@
         </div>
 
          <div id="hamburguer">
-            <img id="foto" src="imagens/456443230_724541109823734_6029369618997251756_n.jpg" alt="Americano">
+            <img id="foto" src="images/imagens/456443230_724541109823734_6029369618997251756_n.jpg" alt="Americano">
             <h3>Americano</h3>
             <p>110g De Blend Burguês, Queijo Cheddar, Fatias De Bacon E O Sabor Especial Do Molho Barbecue No Pão Brioche.</p>
             <strong>R$ 27,90</strong>
         </div>
 
          <div id="hamburguer">
-            <img id="foto" src="imagens/456559587_1176813483607529_5187894326773792844_n.jpg" alt="Costela Gorgonzola">
+            <img id="foto" src="images/imagens/456559587_1176813483607529_5187894326773792844_n.jpg" alt="Costela Gorgonzola">
             <h3>Costela Gorgonzola</h3>
             <p> 180g Blend Costela, Cebola Crispy, Creme De Gorgonzola E Molho A Sua Escolha No Pão Brioche. </p>
             <strong>R$ 35,90</strong>
         </div>
 
          <div id="hamburguer">
-            <img id="foto" src="imagens/456879902_483612124457922_5731631353008052517_n.jpg" alt="Gran Tradicional">
+            <img id="foto" src="images/imagens/456879902_483612124457922_5731631353008052517_n.jpg" alt="Gran Tradicional">
             <h3>Gran Tradicional</h3>
             <p>180g De Blend Burguês, Queijo Muçarela, Cebola Roxa, Alface Americana, Tomate E Molho À Sua Escolha No Pão Brioche. </p>
             <strong>R$ 37,90</strong>
         </div>
 
          <div id="hamburguer">
-            <img id="foto" src="imagens/457126883_1719121988626404_5180914323847251039_n.jpg" alt="Veggie" >
+            <img id="foto" src="images/imagens/457126883_1719121988626404_5180914323847251039_n.jpg" alt="Veggie" >
             <h3>Veggie</h3>
             <p>180g De Blend Burguês, Queijo Mussarela, Cebola Roxa, Alface Americana, Tomate E Molho À Sua Escolha No Pão Brioche. </p>
             <strong>R$ 32,90</strong>
@@ -99,19 +99,19 @@
 <h2 style="color: #E65100">Bebidas:</h2>
 <div id="corpo">
     <div id="hamburguer">
-        <img id="foto" src="imagens/refrigerante-lata.jpg" alt="Coca-Cola Lata">
+        <img id="foto" src="images/imagens/refrigerante-lata.jpg" alt="Coca-Cola Lata">
         <h3>Coca-Cola Lata</h3>
         <p>Geladinha pra acompanhar seu burguer favorito.</p>
         <strong>R$ 6,00</strong>
     </div>
     <div id="hamburguer">
-        <img id="foto" src="imagens/guarana.jpg" alt="Guaraná Antarctica Lata">
+        <img id="foto" src="images/imagens/guarana.jpg" alt="Guaraná Antarctica Lata">
         <h3>Guaraná Antarctica</h3>
         <p>O clássico brasileiro que não pode faltar.</p>
         <strong>R$ 6,00</strong>
     </div>
     <div id="hamburguer">
-        <img id="foto" src="imagens/350036---Bebida-Energetica-Red-Bull-355ml-1.webp" alt="Redbull">
+        <img id="foto" src="images/imagens/350036---Bebida-Energetica-Red-Bull-355ml-1.webp" alt="Redbull">
         <h3>Red bull</h3>
         <p>Opção de energético para quem precisa se manter ativo!</p>
         <strong>R$ 8,00</strong>
@@ -122,19 +122,19 @@
 <h2 style=" color: #E65100">Acompanhamentos:</h2>
 <div id="corpo">
     <div id="hamburguer">
-        <img id="foto" src="imagens/622291-batata-airfryer-extra-croc-mccain_3.webp" alt="Batata Frita">
+        <img id="foto" src="images/imagens/622291-batata-airfryer-extra-croc-mccain_3.webp" alt="Batata Frita">
         <h3>Batata Frita</h3>
         <p>Porção média de batata frita crocante com sal.</p>
         <strong>R$ 24,00</strong>
     </div>
     <div id="hamburguer">
-        <img id="foto" src="imagens/5d2cb094b44ce72d3e3baa89.webp" alt="Onion Rings">
+        <img id="foto" src="images/imagens/5d2cb094b44ce72d3e3baa89.webp" alt="Onion Rings">
         <h3>Onion Rings</h3>
         <p>Anéis de cebola empanados e crocantes.</p>
         <strong>R$ 27,00</strong>
     </div>
     <div id="hamburguer">
-        <img id="foto" src="imagens/mandioca-frita.jpg" alt="Mandioca">
+        <img id="foto" src="images/imagens/mandioca-frita.jpg" alt="Mandioca">
         <h3>Mandioca frita</h3>
         <p>Porção média de mandioca frita crocante.</p>
         <strong>R$ 20,00</strong>
